17b1bf7ab89eeb001f2e3b080fb88e1480343bb9
Wilsonator5000 <wilsonator5000@gmail.com>
2018-08-12 18:30:23 -0400

    Rename underscored folders to dashes to allow Jekyll build

---
+++ -scripts/index.js
@@ -0,0 +1,6 @@
+$(function() {
+    $('#sh-navbar').load('/-components/sh-navbar.html');
+    $('#sh-footer').load('/-components/sh-footer.html');
+    $('#sh-quicklinks').load('/-components/sh-quicklinks.html');
+    $('#sh-underconstruction-banner').load('/-components/sh-underconstruction-banner.html');
+});
--- _scripts/index.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    $('#sh-navbar').load('/_components/sh-navbar.html');
-    $('#sh-footer').load('/_components/sh-footer.html');
-    $('#sh-quicklinks').load('/_components/sh-quicklinks.html');
-    $('#sh-underconstruction-banner').load('/_components/sh-underconstruction-banner.html');
-});
